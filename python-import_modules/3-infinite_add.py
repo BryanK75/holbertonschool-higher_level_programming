da118c69524e30e063b5e4d67da01c7d365798e5
BryanK75 <kotrobryan@gmail.com>
2024-05-10 17:29:32 +0000

    fixed code

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if __name__ == '__main__':
-    av = sys.argv
-    l_av = len(av)
-    sum = 0
-    
-    if l_av > 1:
-        for i in range(1, l_av):
-            sum += int(av[i])
-    print(sum)
+    res = 0
+    len_args = len(sys.argv) - 1
+    for i in range(0, len_args):
+        res += int(sys.argv[i + 1])
+    print("{}".format(res))
