a13c97967d8eebce7fe3b5bbcc90dfd02c480181
BryanK75 <kotrobryan@gmail.com>
2024-05-10 17:23:45 +0000

    fixed code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == '__main__':
-    """
-    Prints the argument list passed to the program
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
-    """
-    av = sys.argv
-    l_av = len(av) - 1
-
-    if l_av > 1:
-        print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 1:
-        print(l_av, 'argument:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 0:
-        print(l_av, 'arguments.')
+if __name__ == "__main__":
+    import sys
+    len_args = len(sys.argv) - 1
+    if len_args == 1:
+        print("{} argument:".format(len_args))
+        print("1: {}".format(sys.argv[1]))
+    elif len_args == 0:
+        print("{} arguments.".format(len_args))
+    else:
+        print("{} arguments:".format(len_args))
+        for i in range(0, len_args):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
