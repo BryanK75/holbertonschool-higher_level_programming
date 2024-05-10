9e775b9d75f22269a980070dcbcb0c7f2a4a4889
BryanK75 <kotrobryan@gmail.com>
2024-05-10 17:28:46 +0000

    fixed code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    num_arguments = len(sys.argv) - 1
-
-    print(f"Number of argument{'s' if num_arguments != 1 else ''}: {'.' if num_arguments == 0 else ''}")
-
-    if num_arguments > 0:
-        for i in range(1, num_arguments + 1):
-            print(f"{i}: {sys.argv[i]}")
-
+    import sys
+    len_args = len(sys.argv) - 1
+    if len_args == 1:
+        print("{} argument:".format(len_args))
+        print("1: {}".format(sys.argv[1]))
+    elif len_args == 0:
+        print("{} arguments.".format(len_args))
+    else:
+        print("{} arguments:".format(len_args))
+        for i in range(0, len_args):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
