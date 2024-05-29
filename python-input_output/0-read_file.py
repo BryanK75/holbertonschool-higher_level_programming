740787213d5316ef86da46bcb23da4b44c103e67
BryanK75 <kotrobryan@gmail.com>
2024-05-29 08:16:22 +0000

    fixed pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
+"""
+    File function for read_file().
+"""
+
+
 def read_file(filename=""):
+    """
+        Reads a file and prints it
+        filename : file to open
+    """
     with open('my_file_0.txt', encoding="utf-8") as file:
-        for line in file:
-            print(line, end ="")
+        print(f"{file.read()}", end="")
