46e7b3100427e764f213fba32135e21df539c523
BryanK75 <kotrobryan@gmail.com>
2024-05-29 09:42:05 +0000

    fixed code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+
+
 """
 File function for read_file().
 """
+
 
 def read_file(filename=""):
     """
     Reads a file and prints its contents to stdout.
 
     Args:
-        filename (str): The name of the file to open and read. Default is an empty string.
+        filename (str): The name of the file to open and read.
     """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
