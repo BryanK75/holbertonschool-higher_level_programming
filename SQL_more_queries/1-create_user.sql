170cec0604c7daf3b1778f15ac090f5aadcb9112
BryanK75 <kotrobryan@gmail.com>
2024-06-20 12:04:11 +0200

    creates a user server in mysql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,11 @@
+-- deletes the user if it already exists
+DROP USER IF EXISTS 'user_01_1'@'localhost';
+
 -- script that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- script that grant all privileges to user user_01_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- flush privileges to apply the changes
+FLUSH PRIVILEGES
