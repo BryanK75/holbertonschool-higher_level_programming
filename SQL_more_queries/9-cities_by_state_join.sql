d7367eb98c0d97dba4687ea9d79de47e38aea527
BryanK75 <kotrobryan@gmail.com>
2024-06-21 10:58:38 +0200

    documentation fixed

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,6 @@
+-- Select cities and their corresponding state names
+-- The output should be ordered by the cities id
+-- The states table has (id, name)
 SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
