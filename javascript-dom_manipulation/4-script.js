009457fb467405e99ad9fc4949eeb3d84cad5b38
BryanK75 <kotrobryan@gmail.com>
2024-07-26 20:14:32 +0200

    fixed

--- javascript-dom_manipulation/4-script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('add_item').addEventListener('click', function() {
-
-    const nItem = document.createElement('li');
-    nItem.textContent = 'Item'
-
-    const list = document.querySelector('ul.my_list');
-    list.appendChild(nItem);
-});
