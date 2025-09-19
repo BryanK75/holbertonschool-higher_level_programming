3c4cbea8a83e6e5ed87595b061b20f60b00ec3d2
BryanK75 <kotrobryan@gmail.com>
2025-09-19 09:12:45 +0200

    pycodestyle fixed

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Class Square that defines a square by its size."""
 
+
 class Square:
     """Defines a square with a private size attribute."""
 
-    def __init__(self, size):
-        """Initialize the square with a given size.
+
+def __init__(self, size):
+    """Initialize the square with a given size.
         Args:
-            size: The size of the square (no type/value verification).
+        size: The size of the square (no type/value verification).
         """
-        self.__size = size
+    self.__size = size
